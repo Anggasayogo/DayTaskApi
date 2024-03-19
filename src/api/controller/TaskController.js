e1e24910b290ccefdeef70f50c34a3b0dc0cceb8
Angga Maulana <angga.maulanaa@visionet.co.id>
2024-03-19 11:26:29 +0700

    fix: get tasklist by users id

--- src/api/controller/TaskController.js
+++ src/api/controller/TaskController.js
@@ -1,13 +1,18 @@
 // Import model Product
+import { QueryTypes } from 'sequelize'
+import db from "../../config/database.js";
 import Task from "../models/Task.model.js";
+import { getTaskByUserId } from "../rawQuery/rawQuery.js";
 
 // Get semua product
 export const getTaskList = async (req, res) => {
   try {
     const taskList = await Task.findAll();
-    res.send(taskList);
+    res.status(200).json({
+      status: 'successfuly',
+      data: taskList
+    });
   } catch (err) {
-    console.log("HELLO",err)
     res.status(500).json({
       error: "Failed Load task",
     });
@@ -37,13 +42,12 @@ export const getTaskById = async (req, res) => {
 
 export const getTaskListByUserId = async (req, res) => {
   try {
-    const product = await Task.findAll({
-      where: {
-        id_pic: req.params.id,
-      },
-    });
+    const product = await db.query(getTaskByUserId(req.params.id), { type: QueryTypes.SELECT })
     if (product?.length > 0) {
-      res.send(product);
+      res.status(200).json({
+        status: 'success',
+        data: product
+      });
     } else {
       res.status(404).json({
         error: "user id not found",
@@ -69,26 +73,37 @@ export const createNassignTask = async (req, res) => {
       id_svp,
     } = req.body;
 
-    if(id_point,task_name,task_progres,task_date,task_duedate,task_docs,id_pic,id_svp){
-        //  create and assign task
-        const newTaskData = {
-            id_point: id_point,
-            task_name: task_name,
-            task_progres: task_progres,
-            task_date: new Date(task_date),
-            task_duedate: new Date(task_duedate),
-            task_docs: task_docs,
-            id_pic: id_pic,
-            id_svp: id_svp,
-        };
-        
-        const task = new Task(newTaskData)
-        await task.save();
-        res.status(201).json({ message: 'Task created successfully', data: newTaskData });
-    }else{
-        res.status(400).json({
-          error: "fields is required",
-        });
+    if (
+      (id_point,
+      task_name,
+      task_progres,
+      task_date,
+      task_duedate,
+      task_docs,
+      id_pic,
+      id_svp)
+    ) {
+      //  create and assign task
+      const newTaskData = {
+        id_point: id_point,
+        task_name: task_name,
+        task_progres: task_progres,
+        task_date: new Date(task_date),
+        task_duedate: new Date(task_duedate),
+        task_docs: task_docs,
+        id_pic: id_pic,
+        id_svp: id_svp,
+      };
+
+      const task = new Task(newTaskData);
+      await task.save();
+      res
+        .status(201)
+        .json({ message: "Task created successfully", data: newTaskData });
+    } else {
+      res.status(400).json({
+        error: "fields is required",
+      });
     }
   } catch (error) {
     res.status(500).json({
