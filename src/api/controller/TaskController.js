a6076beb3148a2967c0a33abca932f75f9869130
Angga Maulana <angga.maulanaa@visionet.co.id>
2024-03-19 10:14:18 +0700

    feat: create task

--- src/api/controller/TaskController.js
+++ src/api/controller/TaskController.js
@@ -1,67 +1,98 @@
 // Import model Product
 import Task from "../models/Task.model.js";
- 
+
 // Get semua product
 export const getTaskList = async (req, res) => {
-    try {
-        const taskList = await Task.findAll();
-        res.send(taskList);
-    } catch (err) {
-        res.status(500).json({ 
-            error: 'Failed Load task' 
-        });
-    }
-}
+  try {
+    const taskList = await Task.findAll();
+    res.send(taskList);
+  } catch (err) {
+    console.log("HELLO",err)
+    res.status(500).json({
+      error: "Failed Load task",
+    });
+  }
+};
 
 export const getTaskById = async (req, res) => {
-    try {
-        const product = await Task.findAll({
-            where: {
-                id: req.params.id
-            }
-        });
-        if(product?.length > 0){
-            res.send(product[0]);
-        }else{
-            res.status(404).json({ 
-                error: 'Task id not found' 
-            });
-        }
-    } catch (err) {
-        res.status(500).json({ 
-            error: 'Failed Load task by id' 
-        });
+  try {
+    const product = await Task.findAll({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (product?.length > 0) {
+      res.send(product[0]);
+    } else {
+      res.status(404).json({
+        error: "Task id not found",
+      });
     }
-}
-
+  } catch (err) {
+    res.status(500).json({
+      error: "Failed Load task by id",
+    });
+  }
+};
 
 export const getTaskListByUserId = async (req, res) => {
-    try {
-        const product = await Task.findAll({
-            where: {
-                id_pic: req.params.id
-            }
-        });
-        if(product?.length > 0){
-            res.send(product);
-        }else{
-            res.status(404).json({ 
-                error: 'user id not found' 
-            });
-        }
-    } catch (err) {
-        res.status(500).json({ 
-            error: 'Failed Load task by user id' 
-        });
+  try {
+    const product = await Task.findAll({
+      where: {
+        id_pic: req.params.id,
+      },
+    });
+    if (product?.length > 0) {
+      res.send(product);
+    } else {
+      res.status(404).json({
+        error: "user id not found",
+      });
     }
-}
-
+  } catch (err) {
+    res.status(500).json({
+      error: "Failed Load task by user id",
+    });
+  }
+};
 
 export const createNassignTask = async (req, res) => {
-    try {
-        console.log(req)
-    } catch (error) {
-        console.log(err);
+  try {
+    const {
+      id_point,
+      task_name,
+      task_progres,
+      task_date,
+      task_duedate,
+      task_docs,
+      id_pic,
+      id_svp,
+    } = req.body;
+
+    if(id_point,task_name,task_progres,task_date,task_duedate,task_docs,id_pic,id_svp){
+        //  create and assign task
+        const newTaskData = {
+            id_point: id_point,
+            task_name: task_name,
+            task_progres: task_progres,
+            task_date: new Date(task_date),
+            task_duedate: new Date(task_duedate),
+            task_docs: task_docs,
+            id_pic: id_pic,
+            id_svp: id_svp,
+        };
+        
+        const task = new Task(newTaskData)
+        await task.save();
+        res.status(201).json({ message: 'Task created successfully', data: newTaskData });
+    }else{
+        res.status(400).json({
+          error: "fields is required",
+        });
     }
-}
- 
+  } catch (error) {
+    res.status(500).json({
+      error: "Error when creating task",
+    });
+  }
+};
--- src/api/models/Task.model.js
+++ src/api/models/Task.model.js
@@ -5,37 +5,43 @@ import db from "../../config/database.js";
  
 // init DataTypes
 const { DataTypes } = Sequelize;
- 
-// Define schema
+
 const Task = db.define('task', {
-  // Define attributes
   id_point: {
-    type: DataTypes.INTEGER
+    type: DataTypes.INTEGER,
+    allowNull: false
   },
   task_name: {
-    type: DataTypes.STRING
+    type: DataTypes.STRING,
+    allowNull: false
   },
   task_progres: {
-    type: DataTypes.STRING
+    type: DataTypes.STRING,
+    allowNull: false
   },
   task_date: {
-    type: DataTypes.DATE
+    type: DataTypes.DATE,
+    allowNull: false
   },
   task_duedate: {
-    type: DataTypes.DATE
+    type: DataTypes.DATE,
+    allowNull: false
   },
   task_docs: {
-    type: DataTypes.STRING
+    type: DataTypes.STRING,
+    allowNull: false
   },
   id_pic: {
-    type: DataTypes.INTEGER
+    type: DataTypes.INTEGER,
+    allowNull: false
   },
   id_svp: {
-    type: DataTypes.INTEGER
-  },
-},{
-  // Freeze Table Name
-  freezeTableName: true
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+}, {
+   // Freeze Table Name
+   freezeTableName: true
 });
  
 // Export model Product
