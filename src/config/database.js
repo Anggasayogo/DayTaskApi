86f0734e5c467914fef488a2385f5a01282c7356
Angga Maulana <angga.maulanaa@visionet.co.id>
2024-07-30 11:15:56 +0700

    fix: db

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,17 @@ import { envStation } from "./envoirmentStation.js";
 // create connection
 const db = new sqlize.Sequelize(envStation?.DB_NAME, envStation?.DB_USERNAME, envStation?.DB_PASSWORD, {
     host: envStation?.DB_HOST,
-    dialect: envStation?.DB_DIALEC
+    dialect: envStation?.DB_DIALEC,
+    port: envStation?.DB_PORT,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    retry: {
+      max: 3,
+    },
 });
  
 // export connection
