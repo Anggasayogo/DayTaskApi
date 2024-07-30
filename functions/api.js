0017856a8f9e4f781283dd3315be3b85df53a08f
Angga Maulana <angga.maulanaa@visionet.co.id>
2024-07-30 13:31:11 +0700

    chore: change data

--- functions/api.js
+++ functions/api.js
@@ -1,44 +1,13 @@
-const express = require('express');
-const serverless = require('serverless-http');
-const app = express();
-const router = express.Router();
+import serverless from 'serverless-http';
+import db from "../src/config/database.js";
+import app from "../src/config/express.js";
 
-let records = [];
+// Testing database connection 
+try {
+    await db.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
 
-
-router.get('/', (req, res) => {
-  res.send('App is running..');
-});
-
-router.post('/add', (req, res) => {
-  res.send('New record added.');
-});
-
-router.delete('/', (req, res) => {
-  res.send('Deleted existing record');
-});
-
-router.put('/', (req, res) => {
-  res.send('Updating existing record');
-});
-
-
-router.get('/demo', (req, res) => {
-  res.json([
-    {
-      id: '001',
-      name: 'Aayush',
-    },
-    {
-      id: '002',
-      name: 'rohit',
-    },
-    {
-      id: '003',
-      name: 'Mohit',
-    },
-  ]);
-});
-
-app.use('/.netlify/functions/api', router);
 module.exports.handler = serverless(app);
