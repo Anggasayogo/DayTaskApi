1de89f046f2a02dc80b459f87a3f8e8537474076
Angga Maulana <angga.maulanaa@visionet.co.id>
2024-08-01 11:03:33 +0700

    chore: emotic

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import app from "./src/config/express.js";
 
 // Testing database connection 
 try {
-    await db.authenticate();
-    console.log('Connection has been established successfully.');
+    const autenticate = await db.authenticate();
+    console.log('🐋 Connection has been established successfully. ');
 } catch (error) {
     console.error('Unable to connect to the database:', error);
 }
